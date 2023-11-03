8ebf43a3a33c8ef28de88baee6bfd318601871f0
FROMERO63 <romerofrank63@gmail.com>
2023-11-02 17:09:30 -0700

    added in login and logout

---
+++ public/javascript/login.js
@@ -0,0 +1,23 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const email = document.querySelector('#emailLogin').value.trim();
+    const password = document.querySelector('#passwordLogin').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.loginForm').addEventListener('submit', loginFormHandler);
